825e351766070d7888cd9b3a388a1a81e72f14d7
Pierre Verdiere <pverdiere59@gmail.com>
2023-06-01 15:52:53 +0200

    feat: fish move + rename directory class to model

--- wator.py
+++ wator.py
@@ -1,8 +1,47 @@
-grid = {}
+import math
+import random
+import time
+from model.fish import Fish
 
-for i in range(8):
+grid = {}
+largeur = 8
+longueur = 15
+listPoisson = []
+
+for i in range(largeur):
     grid[i] = []
-    for j in range(15):
-        grid[i].append("")
-print(grid)
-print(grid[5][10])
+    for j in range(longueur):
+        grid[i].append(" ")
+
+def initFish(largeur,longueur,grid) -> list:
+    nbPoisson = math.floor((longueur/3) * largeur)
+    listPoisson = []
+    for i in range(nbPoisson):
+        ok = False
+        while not(ok):
+            largRandom = random.randrange(largeur)
+            longRandom = random.randrange(longueur)
+            caseSelect = grid[largRandom][longRandom]
+            if caseSelect == " ":
+                grid[largRandom][longRandom] = Fish([largRandom,longRandom])
+                listPoisson.append(grid[largRandom][longRandom])
+                ok = True
+    return listPoisson
+
+listPoisson = initFish(largeur,longueur,grid)
+
+while True:
+    for poisson in listPoisson:
+        positions = poisson.move(grid)
+        poisson.setPosition(positions[1])
+        grid[positions[0][0]][positions[0][1]] = " "
+        grid[positions[1][0]][positions[1][1]] = "§"
+
+    print("_" * (len(grid[0]) + 2))
+    for key, ligne in grid.items():
+        textLigne = ""
+        for col in ligne:
+            textLigne += col if type(col) == str else col.symbol
+        print(f"|{textLigne}|")
+    print("_" * (len(grid[0]) + 2))
+    time.sleep(1)
